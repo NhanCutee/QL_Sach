6fe9a386ca95dca0e23b6c86f60291565789ef44
Nhan <Nhan@DESKTOP-RS6TSTR>
2022-11-04 18:31:17 +0700

    Khai báo thêm DSD_Sach là 1 danh sách đơn

---
+++ QL_Sach/DTO/DSD_Sach.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_Sach.DTO
+{
+    internal class Sach
+    {
+        private string maSach;
+        private string tenSach;
+        private string theLoai;
+        private string tenTacGia;
+        private string nhaXuatBan;
+        private DateTime ngayXuatBan;
+        private string ghiChu;
+        private int gia;
+    }
+    class DSS
+    {
+        public Sach data;
+        int n;
+    }
+
+}
