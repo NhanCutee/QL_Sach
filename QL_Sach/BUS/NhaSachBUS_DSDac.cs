2e42c11447bd28fa39e442a8190d4b2070b9b6fe
Nhan <Nhan@DESKTOP-RS6TSTR>
2022-12-02 21:42:28 +0700

    Thêm NhaSachBus_DSDac, NhaSachBUS_DSLienKet, NhaSachDTO

---
+++ QL_Sach/BUS/NhaSachBUS_DSDac.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QL_Sach.DTO;
+namespace QL_Sach.BUS
+{
+    internal class NhaSachBUS_DSDac
+    {
+        private NhaSachDTO[] nhasachDTO;
+        private int n;
+
+        public List<NhaSachDTO> SachList
+        {
+            get
+            {
+                List<NhaSachDTO> kq = new List<NhaSachDTO>();
+                for (int i = 0; i < this.n; i++)
+                    kq.Add(nhasachDTO[i]);
+                return kq;
+            }
+        }
+        public int N { get => n; set => n = value; }
+
+        public NhaSachBUS_DSDac()
+        {
+            nhasachDTO = new NhaSachDTO[100];
+            n = 0;
+        }
+        public void themSach(NhaSachDTO ns)
+        {
+
+            nhasachDTO[n] = ns;
+            n++;
+        }
+        public void themNhaSach(string enter_maNhaSach, string enter_tenNhaSach, string enter_diaChi, string enter_tenQuanLi, int enter_soLuongNhanVien)
+        {
+     
+            NhaSachDTO ns = new NhaSachDTO();
+            ns.MaNhaSach=enter_maNhaSach;
+            ns.TenNhaSach = enter_tenNhaSach;
+            ns.DiaChi=enter_diaChi;
+            ns.TenQuanLi = enter_tenQuanLi;
+            ns.SoLuongNhanVien = enter_soLuongNhanVien;
+            nhasachDTO[n] = ns;
+            n++;
+        }
+        public void suaSach(int index, string enter_maNhaSach, string enter_tenNhaSach, string enter_diaChi, string enter_tenQuanLi, int enter_soLuongNhanVien)
+        {
+            nhasachDTO[index].MaNhaSach = enter_maNhaSach;
+            nhasachDTO[index].TenNhaSach = enter_tenNhaSach;
+            nhasachDTO[index].DiaChi = enter_diaChi;
+            nhasachDTO[index].TenQuanLi = enter_tenQuanLi;
+            nhasachDTO[index].SoLuongNhanVien = enter_soLuongNhanVien;
+            
+        }
+
+
+        ~NhaSachBUS_DSDac()
+        {
+            nhasachDTO = null;
+        }
+        public void xoaNhaSach(int vt)
+        {
+            for (int i = vt; i < this.n - 1; i++)
+                nhasachDTO[i] = nhasachDTO[i + 1];
+            this.n--;
+        }
+        public void xoaTatCa()
+        {
+            this.n = 0;
+        }
+
+        public int soLuongNhaSach()
+        {
+            return this.n;
+        }
+
+    }
+}
+
---
+++ QL_Sach/BUS/NhaSachBUS_DSLienKet.cs
@@ -0,0 +1,163 @@
+﻿using QL_Sach.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_Sach.BUS
+{
+    class nodeLienKet2
+    {
+        private NhaSachDTO nhasachDTO;
+        private nodeLienKet2 next;
+        private nodeLienKet2 prev;
+
+        public nodeLienKet2()
+        {
+            this.next = null;
+            this.prev = null;
+        }
+
+        public nodeLienKet2(nodeLienKet2 anotherNode)
+        {
+            this.nhasachDTO = anotherNode.nhasachDTO;
+            this.next = anotherNode.next;
+            this.prev = anotherNode.prev;
+
+        }
+        public nodeLienKet2(NhaSachDTO enter_NhaSachDTO)
+        {
+            nhasachDTO = new NhaSachDTO(enter_NhaSachDTO);
+            next = null;
+            prev = null;
+        }
+        public nodeLienKet2(string enter_maNhaSach, string enter_tenNhaSach, string enter_diaChi, string enter_tenQuanLi, int enter_soLuongNhanVien)
+        {
+            nhasachDTO = new NhaSachDTO(enter_maNhaSach,  enter_tenNhaSach, enter_diaChi, enter_tenQuanLi, enter_soLuongNhanVien);
+            next = null;
+            prev = null;
+        }
+        ~nodeLienKet2() { }
+
+        public NhaSachDTO NhaSachDTO { get => nhasachDTO; set => nhasachDTO = value; }
+        public nodeLienKet2 Next { get => next; set => next = value; }
+        public nodeLienKet2 Prev { get => prev; set => prev = value; }
+    }
+    class NhaSachBUS_DSLienKet
+    {
+        private int count;
+        private nodeLienKet2 firstNode;
+
+        public List<NhaSachDTO> NhaSachList
+        {
+            get
+            {
+                List<NhaSachDTO> nhasachList = new List<NhaSachDTO>();
+                for (nodeLienKet2 node = firstNode; node.Next != null; node = node.Next)
+                    nhasachList.Add(node.NhaSachDTO);
+                return nhasachList;
+            }
+        }
+
+        public NhaSachBUS_DSLienKet()
+        {
+            count = 0;
+            firstNode = new nodeLienKet2();
+        }
+        public void themNhaSach(NhaSachDTO ns)
+        {
+            nodeLienKet2 node = new nodeLienKet2(ns);
+            node.Next = firstNode;
+            firstNode.Prev = node;
+            firstNode = node;
+            count++;
+        }
+
+        public void themNhaSach(string enter_maNhaSach, string enter_tenNhaSach, string enter_diaChi, string enter_tenQuanLi, int enter_soLuongNhanVien)
+        {
+            nodeLienKet2 node = new nodeLienKet2(enter_maNhaSach, enter_tenNhaSach, enter_diaChi, enter_tenQuanLi, enter_soLuongNhanVien);
+            node.Next = firstNode;
+            firstNode.Prev = node;
+            firstNode = node;
+            count++;
+        }
+
+        public void suaNhaSach(int index, string enter_maNhaSach, string enter_tenNhaSach, string enter_diaChi, string enter_tenQuanLi, int enter_soLuongNhanVien)
+        {
+            nodeLienKet2 node = this.firstNode;
+            for (int i = 0; i < index; i++)
+                node = node.Next;
+            node.NhaSachDTO.MaNhaSach = enter_maNhaSach;
+            node.NhaSachDTO.TenNhaSach = enter_tenNhaSach;
+            node.NhaSachDTO.DiaChi = enter_diaChi;
+            node.NhaSachDTO.TenQuanLi = enter_tenQuanLi;
+            node.NhaSachDTO.SoLuongNhanVien = enter_soLuongNhanVien;
+            
+     
+        }
+        //check
+        public void suaNhaSach(string enter_maNhaSach, string enter_tenNhaSach, string enter_diaChi, string enter_tenQuanLi, int enter_soLuongNhanVien)
+        {
+            nodeLienKet2 node = this.firstNode;
+            while (node != null)
+            {
+                if (node.NhaSachDTO.MaNhaSach == enter_maNhaSach)
+                    break;
+                node = node.Next;
+            }
+            node.NhaSachDTO.TenNhaSach = enter_tenNhaSach;
+            node.NhaSachDTO.DiaChi =enter_diaChi;
+            node.NhaSachDTO.TenQuanLi= enter_tenQuanLi;
+            node.NhaSachDTO.SoLuongNhanVien= enter_soLuongNhanVien;
+           
+        }
+        //check
+        public void xoaNhaSach(int index)
+        {
+            nodeLienKet2 node = this.firstNode;
+            nodeLienKet2 nodeTemp = new nodeLienKet2();
+            for (int i = 0; i < index - 1; i++)
+                node = node.Next;
+            nodeTemp = node.Next;
+            node.Next = node.Next.Next;
+            nodeTemp = null;
+        }
+        public void xoaNhaSach(string enter_maNhaSach)
+        {
+            nodeLienKet2 node = this.firstNode;
+            nodeLienKet2 nodeTemp;
+            while (node != null)
+            {
+                if (node.NhaSachDTO.MaNhaSach== enter_maNhaSach)
+                {
+                    nodeTemp = node;
+                    node = node.Prev;
+                    node.Next = nodeTemp.Next;
+                    nodeTemp = null;
+                    break;
+                }
+                node = node.Next;
+            }
+        }
+
+        public void xoaTatCa()
+        {
+            nodeLienKet2 node = this.firstNode;
+            nodeLienKet2 nodeTemp;
+            while (node != null)
+            {
+                nodeTemp = node;
+                node = node.Next;
+                nodeTemp = null;
+            }
+        }
+
+        public int soLuongNhaSach()
+        {
+            return count;
+        }
+
+
+    }
+}
